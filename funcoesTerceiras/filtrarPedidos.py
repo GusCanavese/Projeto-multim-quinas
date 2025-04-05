ac8b3190d89af95b724a2aa4d0b4f31b790a9504
gustavo <bicalhogustavo866@gmail.com>
2025-04-04 21:00:43 -0300

    .

--- funcoesTerceiras/filtrarPedidos.py
+++ funcoesTerceiras/filtrarPedidos.py
@@ -21,10 +21,7 @@ def filtrarPedidos(self, vendedor, numero):
             pedido[1],
             pedido[3]   
         ]
-        
-        # 
-        # 
-        
+
         for colNum, valor in enumerate(dados):
             dado = ctk.CTkLabel(self.frameParaVendasNoRelatorio, text=valor, width=150, fg_color="#38343c", anchor="center")
             dado.grid(row=rowPedido, column=colNum, padx=2, pady=2)
