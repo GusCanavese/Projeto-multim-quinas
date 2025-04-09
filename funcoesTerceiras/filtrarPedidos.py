26abfe46ae2b01a47bded153d661a6c61bd4d744
gustavo <bicalhogustavo866@gmail.com>
2025-04-08 21:31:43 -0300

    alterações na tela de relatorio de vendas

--- funcoesTerceiras/filtrarPedidos.py
+++ funcoesTerceiras/filtrarPedidos.py
@@ -6,21 +6,38 @@ from consultas.select import Buscas
 import tkinter as tk
 from tkcalendar import DateEntry
 
-def filtrarPedidos(self, vendedor, numero):
-    print(vendedor)
-    print(numero)
 
-    pedidos = Buscas.buscaPedidos()
+def filtrarPedidos(self, vendedor, numero, inicio, fim, checkbox):
+    print(checkbox)
+    pedidos = Buscas.buscaPedidos(vendedor, numero, inicio, fim, checkbox)
+
+    if hasattr(self, "dados"):
+        for self.labelDadosPedido in self.dados:
+            self.labelDadosPedido.destroy()
+            print("entrou")
+        self.dados=[]
+
+
     print(pedidos)
-    
-    for rowPedido, pedido in enumerate(pedidos, start=1):  
 
-        dados = [
-            pedido[0],  # Item
+    self.dados=[]
+    
+    for rowPedido, pedido in enumerate(pedidos, start=1):
+        self.valores = [
+            pedido[0], 
             pedido[2],
             pedido[1],
             pedido[3]   
         ]
+<<<<<<< HEAD
         for colNum, valor in enumerate(dados):
             dado = ctk.CTkLabel(self.frameParaVendasNoRelatorio, text=valor, width=150, fg_color="#38343c", anchor="center")
             dado.grid(row=rowPedido, column=colNum, padx=2, pady=2)
+=======
+
+        for colNum, valor in enumerate(self.valores):
+            self.labelDadosPedido = ctk.CTkLabel(self.frameParaVendasNoRelatorio, text=valor, width=150, fg_color="#38343c", anchor="center")
+            self.labelDadosPedido.grid(row=rowPedido, column=colNum, padx=2, pady=2)
+            self.dados.append(self.labelDadosPedido)
+            
+>>>>>>> e96a87f (alterações na tela de relatorio de vendas)
