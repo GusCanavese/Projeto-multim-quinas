d487dec792c76de05ae396f000df076223805683
Gustavo <bicalhogustavo866@gmail.com>
2025-05-24 11:10:10 -0300

    finalizado

--- telas/telagerarFaturamento.py
+++ telas/telagerarFaturamento.py
@@ -15,7 +15,7 @@ def telaGerarFaturamento(self):
     posicaoy = 0.1
     self.y=0.138
     self.teste =0.038
-    self.yParcelas = 0.1
+    self.yParcelas = 0.138
 
     self.botaoAdicionarParcela = ctk.CTkButton(self.frameTelaGerarFaturamento, text="Adicionar Parcela", width=20, corner_radius=0, command=lambda:verificaParcelasPreenchidas(self)) 
 
@@ -27,7 +27,6 @@ def telaGerarFaturamento(self):
 
     def adicionaParcela(self):
         self.y += 0.038
-        self.yParcelas += 0.038
         self.botaoAdicionarParcela.place(relx=0.2, rely=self.y)  # se botao já for salvo
 
         self.combobox = ctk.CTkComboBox(self.frameTelaGerarFaturamento, width=100, values=opcoesPagamento, corner_radius=0, command=lambda valor: modal(self, valor))
@@ -44,13 +43,17 @@ def telaGerarFaturamento(self):
     adicionaParcela(self)
 
     def removerParcela(self):
-        if self.row <2:
+        if self.row ==1:
+            self.botaoRemoverParcela.destroy()
+            del self.botaoRemoverParcela
             print("primeira linha ja encontrada")
             print(self.row)
             pass
         else:
-            
-            print("teste")
+            if self.row ==1 and hasattr(self, "botaoRemoverParcela"):
+                self.botaoRemoverParcela.destroy()
+                del self.botaoRemoverParcela
+                print("teste")
             self.y-=0.038
             self.teste -=0.038
             self.yParcelas -=0.038
@@ -78,13 +81,16 @@ def telaGerarFaturamento(self):
 
     def verificaParcelasPreenchidas(self):
         if (self.listaComboboxes[self.row-1].get() and self.listaEntradaQuantidade[self.row-1].get() and self.listaEntradaValor[self.row-1].get()):
-            print(self.row)
+            
             self.row +=1
+            print(self.row)
 
             if hasattr(self, "botaoRemoverParcela") and self.botaoRemoverParcela.winfo_exists():
                 self.botaoRemoverParcela.destroy()
             self.botaoRemoverParcela = ctk.CTkButton(self.frameTelaGerarFaturamento, text="X", width=20, corner_radius=0, fg_color="red", command=lambda: removerParcela(self))
-            self.botaoRemoverParcela.place(relx=0.8, rely=self.yParcelas + self.teste)
+            self.yParcelas += 0.038
+            
+            self.botaoRemoverParcela.place(relx=0.8, rely=self.yParcelas)
             adicionaParcela(self)
 
         else:
