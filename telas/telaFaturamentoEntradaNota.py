715681edcf730b20b2429c67c4addfdcbc15756a
GustavoPC <bicalhogustavo866@email.com>
2025-10-25 17:02:47 -0300

    merge branch 'master' into 'gabriel'

--- telas/telaFaturamentoEntradaNota.py
+++ telas/telaFaturamentoEntradaNota.py
@@ -10,7 +10,7 @@ from datetime import date
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
-def telaGerarFaturamentoEntradaNota(self, DadosNota, valorNota, cons):
+def telaGerarFaturamentoEntradaNota(self, DadosNota, valorNota, EhNotaDoConsumidor):
 
     self.variavelRepeticao = 0
     self.row = 1
